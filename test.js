03121011c8174f5090be9406478707045e92c536
wood <zhouwuwang8@126.com>
2015-05-16 21:23:24 +0800

    modify

---
+++ test.js
@@ -0,0 +1,47 @@
+/**
+ * Created by wood on 5/16/15.
+ */
+var wechat = require('wechat');
+var config = {
+    token: 'woodwechattest',
+    appid: 'wx2ed9d8d807bfe6bf',
+    encodingAESKey: 'Q5sy8ce8UhXiM7qs3FYwN0N9M97FaLnJVctmNKJi0nU'
+};
+
+app.use(connect.query()); // Or app.use(express.query());
+app.use('/wechat', wechat(config, function (req, res, next) {
+    // 微信输入信息都在req.weixin上
+    var message = req.weixin;
+    if (message.FromUserName === 'diaosi') {
+        // 回复屌丝(普通回复)
+        res.reply('hehe');
+    } else if (message.FromUserName === 'text') {
+        //你也可以这样回复text类型的信息
+        res.reply({
+            content: 'text object',
+            type: 'text'
+        });
+    } else if (message.FromUserName === 'hehe') {
+        // 回复一段音乐
+        res.reply({
+            type: "music",
+            content: {
+                title: "来段音乐吧",
+                description: "一无所有",
+                musicUrl: "http://mp3.com/xx.mp3",
+                hqMusicUrl: "http://mp3.com/xx.mp3",
+                thumbMediaId: "thisThumbMediaId"
+            }
+        });
+    } else {
+        // 回复高富帅(图文回复)
+        res.reply([
+            {
+                title: '你来我家接我吧',
+                description: '这是女神与高富帅之间的对话',
+                picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
+                url: 'http://nodeapi.cloudfoundry.com/'
+            }
+        ]);
+    }
+}));
